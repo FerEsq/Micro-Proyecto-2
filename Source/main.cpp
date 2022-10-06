a11839e77a0e6bbfff3f5643cd950c487899c79f
Francisco Castillo <cas21562@uvg.edu.gt>
2022-10-06 07:55:38 -0600

    Ask user for DNA sequence

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,12 +1,33 @@
 /*
- * ENCABEZADO  
+ * ENCABEZADO
  *
  *
  */
 
 #include <iostream>
+using namespace std;
+
+const int CHARS_PER_THREAD = 10;
+
+void separator();
+string askDNASequence();
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string adnInput = askDNASequence();
+    pthread_t threads[adnInput.length() / CHARS_PER_THREAD];
     return 0;
+}
+
+void separator(){
+    cout << "=============================================================" << endl;
+}
+
+string askDNASequence(){
+    separator();
+    string temp;
+    cout << "Ingrese la cadena de ADN que desea traducir:" << endl;
+    cin >> temp;
+    separator();
+    return temp;
 }
