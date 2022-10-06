2d253fdcc5fab122ab7055814ffd00e6f5af89de
Fernanda Esquivel <esq21542@uvg.edu.gt>
2022-10-06 08:00:42 -0600

    Documentation and variables

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,13 +1,25 @@
 /*
- * ENCABEZADO
- *
- *
- */
+	------------------------------------------------------------------
+	UNIVERSIDAD DEL VALLE DE GUATEMALA
+	Programacion de Microprocesadores
+	Ciclo 2 - 2022
+
+	main.cpp
+
+	Fernanda Esquivel - 21542
+	Francisco Castillo - 21562
+	Andres Montoya - 21552
+    Diego Lemus - 21469
+	--------------------------------------------------------------------
+*/
 
 #include <iostream>
 using namespace std;
 
 const int CHARS_PER_THREAD = 10;
+string arnTranscription;
+int arnPos;
+string aminoacids;
 
 void separator();
 string askDNASequence();
