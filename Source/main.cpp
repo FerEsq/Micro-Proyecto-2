f12cddfd390660058debbdfe72cc659ce1354388
Francisco Castillo <cas21562@uvg.edu.gt>
2022-10-06 08:26:33 -0600

    First ARN Transcription

--- Source/main.cpp
+++ Source/main.cpp
@@ -16,18 +16,35 @@
 #include <iostream>
 using namespace std;
 
+struct stringPosition{
+    int start;
+    int end;
+};
+
 const int CHARS_PER_THREAD = 10;
 string arnTranscription;
-int arnPos;
+int arnPos, nThreads;
 string aminoacids;
 
 void separator();
 string askDNASequence();
-
+void* makeARNtranscription(void *args);
 
 int main() {
     string adnInput = askDNASequence();
-    pthread_t threads[adnInput.length() / CHARS_PER_THREAD];
+    nThreads = adnInput.length() / CHARS_PER_THREAD;
+    pthread_t threads[nThreads];
+    // INITIALIZES THE PTHREAD
+    int threadPos = 0;
+    stringPosition adnPos{};
+    for(auto thread: threads){
+        // 0 - 10 → 10 - 20 → 20 - 30
+        adnPos = {CHARS_PER_THREAD*threadPos, CHARS_PER_THREAD*threadPos+1};
+        pthread_create(&threads[threadPos++], nullptr, &makeARNtranscription, (void*) &adnPos);
+    }
+    for(auto thread: threads){
+        pthread_join(thread, nullptr);
+    }
     return 0;
 }
 
@@ -42,4 +59,8 @@ string askDNASequence(){
     cin >> temp;
     separator();
     return temp;
+}
+
+void* makeARNtranscription(void *args){
+    
 }
